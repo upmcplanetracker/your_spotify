f2ff65ee2e3aec973ba556f34454e35b9846f0bc
upmcplanetracker <219436948+upmcplanetracker@users.noreply.github.com>
2026-08-28 10:48:23 -0400

    Update serve.sh to listen on all interfaces dual-stack

--- apps/client/scripts/run/serve.sh
+++ apps/client/scripts/run/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # -s means that all 404's will be redirected to index.html, so that react can handle router
-# -l 0.0.0.0 means that it's hosted on all the interfaces
+# -l tcp://[::]:3000 listens on all interfaces dual-stack (IPv6 + IPv4)
 # build/ is the output of the package built at build-time
 
-exec serve -c /app/apps/client/scripts/run/serve.json -s -l tcp://0.0.0.0:3000 /app/apps/client/build/
+exec serve -c /app/apps/client/scripts/run/serve.json -s -l tcp://[::]:3000 /app/apps/client/build/
